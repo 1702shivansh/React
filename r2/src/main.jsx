7498a03a69785bf839e69faeab35006f8d3aadd3
1702shivansh <shivanshgrover17022004@gmail.com>
2025-09-01 02:12:15 +0530

    understood the internal working of react

--- r2/src/main.jsx
+++ r2/src/main.jsx
@@ -1,10 +1,33 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import React from 'react'
+import {createRoot} from 'react-dom/client'
 import App from './App.jsx'
 
+// function MyApp(){
+//   return <div>
+//     <h1>hi from Shivansh</h1>
+//   </div>
+// }
+// const reactElement={
+//     type:"a",
+//     props:{
+//         href:"https://google.com",
+//         target:"_blank",
+//     },
+//     children:"click me to visit google",
+// };
+//a plane object
+// const anotherReactElement=(
+//   <a href='https://google.com' target='_blank' >Visit Google</a>
+// )
+//now the way in which react create sthe element is as
+const reactElement=React.createElement(
+  'a',{href:"https://google.com",target:"_blank"},
+  'click me to visit google',
+)
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+    // <App />
+    // <MyApp/>
+    // reactElement // it wont work because the way in which react interprets the synatx can be different 
+    // anotherReactElement//it works definitely 
+     reactElement // the way in which React understand the syntax and then converts
 )
